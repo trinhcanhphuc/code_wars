67a252c9cbc9153c1a09654d393dcd149b7db411
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-05-10 10:40:48 +0700

    update permutations.py

--- permutations.py
+++ permutations.py
@@ -1,3 +1,36 @@
+"""
+Description:
+In this kata you have to create all permutations of an input string and remove duplicates, if present. This means, you have to shuffle all letters from the input in all possible orders.
+
+Examples:
+
+permutations('a'); // ['a']
+permutations('ab'); // ['ab', 'ba']
+permutations('aabb'); // ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+permutations("a"); // => vector<string> {"a"}
+permutations("ab"); // => vector<string> {"ab", "ba"}
+permutations("aabb"); // => vector<string> {"aabb", "abab", "abba", "baab", "baba", "bbaa"}
+permutations('a'); // => ['a']
+permutations('ab'); // => ['ab', 'ba']
+permutations('aabb'); // => ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+permutations('a'); # ['a']
+permutations('ab'); # ['ab', 'ba']
+permutations('aabb'); # ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+permutations('a'); # ['a']
+permutations('ab'); # ['ab', 'ba']
+permutations('aabb'); # ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+permutations    "a" `shouldBe` ["a"]
+permutations   "ab" `shouldBe` ["ab", "ba"]
+permutations "aabb" `shouldBe` ["aabb","abab","abba","baab","baba","bbaa"]
+Permutations.singlePermutations("a") `shouldBe` ["a"]
+Permutations.singlePermutations("ab") `shouldBe` ["ab", "ba"]
+Permutations.singlePermutations("aabb") `shouldBe` ["aabb","abab","abba","baab","baba","bbaa"]
+Permutations.SinglePermutations("a"); // => new List {"a"}
+Permutations.SinglePermutations("ab"); // => new List {"ab", "ba"}
+Permutations.SinglePermutations("aabb"); // => new List {"aabb", "abab", "abba", "baab", "baba", "bbaa"}
+The order of the permutations doesn't matter.
+"""
+
 def permutations(string):
     result = set([string])
     if len(string) == 2:
